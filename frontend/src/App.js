16b131643eab200083164cb329d743a54136ce8a
Cdemz <gbengaadepoju75@gmail.com>
2023-04-29 12:14:23 +0100

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+       
+      </header> 
+      <main>
+          list Product
+        </main>
+    </div>
+  );
+}
+
+export default App;
