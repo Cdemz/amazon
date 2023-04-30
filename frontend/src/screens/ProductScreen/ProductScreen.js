29a4544f2e8599c264f19bff2a4e28b3f3e81efe
Cdemz <gbengaadepoju75@gmail.com>
2023-04-30 22:59:38 +0100

    i am coming back

--- frontend/src/screens/ProductScreen/ProductScreen.js
+++ frontend/src/screens/ProductScreen/ProductScreen.js
@@ -1,11 +1,66 @@
+import axios from 'axios';
+import { useEffect, useReducer } from 'react';
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
 import { useParams } from 'react-router-dom';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, product: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
 function ProductScreen() {
-  const param = useParams();
-  const { slug } = param;
-  return (
+  const params = useParams();
+  const { slug } = params;
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: '',
+  });
+  // const [products, setProduct] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get(`/api/products/slug${slug}`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+    };
+    fetchData();
+  }, [slug]);
+
+  return loading ? (
+    <div>Loading...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
     <div>
-      <h1>{slug}</h1>
+      <Row>
+        <Col md={6}>
+          <img
+            src={product.image}
+            className="img-large"
+            alt={product.name}
+          ></img>
+        </Col>
+        <Col md={3}>
+          <h1>{product.name}</h1>
+          <p>{product.description}</p>
+        </Col>
+        <Col md={3}>
+          <h1>{product.price}</h1>
+        </Col>
+      </Row>
     </div>
   );
 }
