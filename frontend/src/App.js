ad6464403baf630ba0865ed611d54476f6aeaa4b
Cdemz <gbengaadepoju75@gmail.com>
2023-04-30 02:02:03 +0100

    added bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
 import ProductScreen from './screens/ProductScreen/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import LinkContainer from 'react-router-bootstrap/LinkContainer';
 
+import Container from 'react-bootstrap/Container';
+import React from 'react';
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <header>
-          <Link to="/">Amazona</Link>
-        </header>
+      <div className="d-flex flex-column site-container">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand href="#home">Amazon</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
+
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+
+        <footer>
+          <div className="text-center">All Right Reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
